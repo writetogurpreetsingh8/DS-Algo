ecc923d66426038c00be8dfcb6d0f4812c574bc6
91783 <91783@LAPTOP-D5EB7VM6>
2019-10-13 00:09:32 +0530

    commit sortings

---
+++ sortings/src/main/java/sortings/selection/Selection.java
@@ -0,0 +1,50 @@
+package sortings.selection;
+
+public class Selection {
+
+	
+	void selectionSort(int array[]) {
+		
+		int minValueIndexIntoArray = 0;
+		int temp = 0;
+		
+		for(int i=0;i<array.length-1;i++) {
+			
+			minValueIndexIntoArray = i;
+			
+			for(int j=i+1;j<array.length;j++) {
+				
+				if(array[minValueIndexIntoArray] > array[j]) {
+					minValueIndexIntoArray = j;
+				}
+			}
+			if(minValueIndexIntoArray != i) {
+				temp = array[i];
+				array[i] = array[minValueIndexIntoArray];
+				array[minValueIndexIntoArray] = temp;
+			}
+			else {
+				break; // if array is already sorted in order
+			}
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int array[]= {5,3,10,2,1};
+		
+		System.out.println("before applied selection sorting array elements are ........");
+		for(int i :array) {
+			System.out.print(i+"\t");
+		}
+		new Selection().selectionSort(array);
+		System.out.println("\n");
+		System.out.println("after applied selection sorting array elements ........");
+		for(int i :array) {
+			System.out.print(i+"\t");
+		}
+	}
+
+}
