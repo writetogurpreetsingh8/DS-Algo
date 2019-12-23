f4146b8494cad3a297573d1848af14648fb738b3
writetogurpreetsingh8 <48581444+writetogurpreetsingh8@users.noreply.github.com>
2019-12-23 16:04:54 +0530

    reverse linked-list using recursive approach

---
+++ algo.practice/src/algo/practice/ReverseLinkedListRecursive.java
@@ -0,0 +1,46 @@
+package hacker.rank;
+
+public class ReverseLinkedListRecursive {
+
+	
+	void displayDataInReverseOrder(Node list){
+	
+		if(list.getNext() == null){
+			return ;
+		}
+		displayDataInReverseOrder(list.getNext());
+		System.out.println("reverse display data "+list.getData());
+	}
+	
+	Node reverseLinkedList(Node list){
+		
+		if(list == null){
+			return list;
+		}
+		if(list.getNext() == null){
+			return list;
+		}
+		Node headNode = reverseLinkedList(list.getNext());
+		list.getNext().setNext(list);
+		list.setNext(null);
+		return headNode;
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		LinkingList linkingList = new LinkingList();
+		Node head = linkingList.createLinkedList();
+		ReverseLinkedListRecursive linkedList = new ReverseLinkedListRecursive();
+		System.out.println("before reverse the list "+head);
+		head = linkedList.reverseLinkedList(head);
+		System.out.println("after reverse the list "+head);
+		System.out.println();
+		
+
+	}
+
+}
+
