69523df22f60a46f2f33d0f093d0e177d89f80ef
writetogurpreetsingh8 <writetogurpreetsingh8@gmail.com>
2020-07-23 14:21:31 +0530

    tower of hanoi reucursively solevDDDddddd

---
+++ algo.practice/src/main/java/algo/practice/TowerOfHanoi.java
@@ -0,0 +1,25 @@
+package algo.practice;
+
+public class TowerOfHanoi {
+
+	private static int call;
+	
+	private static void TOH(int plates,char sourcePiller,char distinationPiller,char auxPiller) {
+		
+		call++;
+		
+		if(plates == 1) {
+			System.out.println("move plate "+plates+" from sourece "+sourcePiller+" to "+distinationPiller);
+			return;
+		}
+		TOH(plates-1,sourcePiller,auxPiller,distinationPiller);
+		System.out.println("move plate "+plates+" from sourece "+sourcePiller+" to "+distinationPiller);
+		TOH(plates-1,auxPiller,distinationPiller,sourcePiller);
+	}
+	public static void main(String[] args) {
+		
+		TowerOfHanoi.TOH(3,'S','D','H');
+		System.out.println("total moves... "+call);
+	}
+
+}
