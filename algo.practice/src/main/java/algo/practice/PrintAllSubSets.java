d98a85bb7f9278c34d6ec922f721f0716a505035
writetogurpreetsingh8 <writetogurpreetsingh8@gmail.com>
2020-08-08 13:41:27 +0530

    print all sub-sets.

---
+++ algo.practice/src/main/java/algo/practice/PrintAllSubSets.java
@@ -0,0 +1,47 @@
+package algo.practice;
+
+import java.util.Date;
+
+public class PrintAllSubSets {
+
+	private static int count;
+	
+	private static void printAllSubSets(String string) {
+	
+		if(string == null || string.isEmpty()) {
+			System.out.println("invalid string.......");
+			return;
+		}
+		
+		print(string,"");
+	}
+	
+	private static void print(String input,String output) {
+		if(input.isEmpty()) {
+			System.out.println(output);
+			return;
+		}
+		count++;	
+		String output1 = output;
+		String output2 = output;
+		
+		output2 = output2+input.charAt(0);
+		
+		input = input.substring(1);
+		
+		print(input, output1);
+		print(input, output2);
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(new Date());
+		
+		PrintAllSubSets.printAllSubSets("abcdeghjiklmnopqrstuvwxyz");
+		System.out.println("total sub-sets are "+count);
+		
+		System.out.println(new Date());
+		
+	}
+
+}
