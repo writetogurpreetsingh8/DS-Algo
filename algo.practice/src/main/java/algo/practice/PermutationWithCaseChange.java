10abcf7c452ba456bb0cee1e2b6fce29f2180527
writetogurpreetsingh8 <writetogurpreetsingh8@gmail.com>
2020-08-08 15:43:08 +0530

    permutation with case change

---
+++ algo.practice/src/main/java/algo/practice/PermutationWithCaseChange.java
@@ -0,0 +1,27 @@
+package algo.practice;
+
+public class PermutationWithCaseChange {
+
+	private static void permutationWithCaseChange(String input,String output) {
+		
+		if(input.length() == 0 ) {
+			System.out.println(output);
+			return;
+		}
+		
+		String output1 = (output+input.charAt(0));
+		String output2 = (output+Character.toUpperCase(input.charAt(0)));
+		
+		input = input.substring(1);
+		
+		permutationWithCaseChange(input,output1);
+
+		permutationWithCaseChange(input,output2);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		PermutationWithCaseChange.permutationWithCaseChange("abc", "");
+	}
+
+}
