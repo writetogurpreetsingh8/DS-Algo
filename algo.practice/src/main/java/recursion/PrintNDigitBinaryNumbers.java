f1a16d376b108678affd1b636e8a88d900ce6ad4
writetogurpreetsingh8 <writetogurpreetsingh8@gmail.com>
2020-08-24 19:14:23 +0530

    print n digit binary numbers...

---
+++ algo.practice/src/main/java/recursion/PrintNDigitBinaryNumbers.java
@@ -0,0 +1,45 @@
+package recursion;
+
+public class PrintNDigitBinaryNumbers {
+
+	
+	private static int counter;
+
+	
+	private static void printNDigitBinaryNumbers(int n) {
+		
+		if(n<1) {
+			System.out.println("invalid given N....... Number must be greater then or equal to 1");
+			return;
+		}
+		printNDigitBinaryNumbers(n,"");
+	}
+	
+	private static void printNDigitBinaryNumbers(int n,String output) {
+		
+		if(n == 0) {
+			System.out.println(output);
+			return;
+		}
+		counter++;
+		
+		String output1 = output;
+		String output2 = output;
+		
+		output1 = output1+"1";
+		output2 = output2+"0";
+		n = n-1;
+		printNDigitBinaryNumbers(n,output1);
+		printNDigitBinaryNumbers(n,output2);
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		PrintNDigitBinaryNumbers.printNDigitBinaryNumbers(5);
+		System.out.println("total passess are "+counter);
+
+	}
+
+}
